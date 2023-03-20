7801752312fbb98f4f99e3ea830b4b67f60dce4f
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-03-20 13:57:48 +0200

    Calculator class app

--- Main.py
+++ Main.py
@@ -1,3 +1,20 @@
 class Calculator:
+    nr = 0
+
     def __init__(self, nr):
-        
+        self.nr = nr
+
+    def multiplication(self, other):
+        self.nr *= other
+
+    def division(self, other):
+        self.nr /= other
+
+    def output_result(self):
+        print(self.nr)
+
+
+c = Calculator(4)
+c.multiplication(3)
+c.division(2)
+c.output_result()
